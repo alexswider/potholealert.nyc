a644b6dcb099258e740250e20ea9102cb74efc03
Przemyslaw Swiderski <alex@biegademarketing.com>
2016-11-08 17:57:46 -0500

    firebase integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,38 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
-import { StatusBar, Splashscreen } from 'ionic-native';
+import { StatusBar } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+
+import firebase from 'firebase';
 
 
 @Component({
   template: `<ion-nav [root]="rootPage"></ion-nav>`
 })
 export class MyApp {
-  rootPage = HomePage;
+  rootPage: any = HomePage;
 
   constructor(platform: Platform) {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBwEUe6x_w_yLFrr--xYLQJLxRT2Rc8vtY",
+      authDomain: "ionic-firebase-auth-9f555.firebaseapp.com",
+      databaseURL: "https://ionic-firebase-auth-9f555.firebaseio.com",
+      storageBucket: "ionic-firebase-auth-9f555.appspot.com",
+      messagingSenderId: "904481277327"
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        this.rootPage = LoginPage;
+      }
+    });
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
-      Splashscreen.hide();
     });
   }
 }
